2cb5aedd0c59c493a309bc580d6cba0460c1381d
Joseph Lubrano <jlubranos@optimum.net>
2020-09-24 12:45:08 -0400

    Update Homework Sept 24

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,8 +1,16 @@
+# main.py 
+# PyPoll Vote counting program
+#   Opens cvs file PyPoll_Analysis.txt
+#   Py_Poll a list of dictionaries is created and intialized.
+#       Py_Poll key=candidate name : vote counter for that candidate
+
 import os
 import csv
 
 Py_Poll = {}
+# Tvotes is the counter for total overall votes.
 Tvotes=0
+# Cname is candidate name which is used to as the key in the dictionary
 Cname=""
 inputpath=os.path.join("Resources","election_data.csv")
 with open(inputpath) as inputfile:
@@ -10,12 +18,22 @@ with open(inputpath) as inputfile:
     inputheader=next(inputrecord)
     for record in csv.reader(inputfile):
         Cname=record[2]
+# Search the list of dictionaries for the candidate name using .get
         if not Py_Poll.get(Cname):
+# If candidate does not exist then create a dictionary and update his vote count to 1.
             Py_Poll[Cname]=1
         else:
+# If candidate name is found then update his vote count by adding 1.
             Py_Poll[Cname]+=1
+# Update overall vote count by 1 as well
         Tvotes+=1
+
+# After processing is complete sort the list of dictionary using the candidate vote count as the value
+# to sort on from largest to smallest so that the winner will be the first dictionary in the list.
     Sorted_Py_Poll=sorted(Py_Poll.items(), reverse=True, key=lambda x : x[1])
+
+# Create PyPoll_Analysis.txt and write the results into the file.
+
 outputpath=os.path.join("Analysis","PyPoll_Analysis.txt")
 outputfile=open(outputpath,"w")
 outputfile.write("Election Results\n")
@@ -23,6 +41,8 @@ outputfile.write("-------------------------\n")
 ctvotes="{:,.0f}".format(float(Tvotes))
 outputfile.write("Total Votes : " + ctvotes + "\n")
 outputfile.write("-------------------------\n")
+
+# Loop to wriite out all the names of the candidates which were created in the list.
 for name,votes in Sorted_Py_Poll:
     pvotes="{:.3%}".format(float(votes/Tvotes))
     ctvotes="{:,.0f}".format(float(votes))
