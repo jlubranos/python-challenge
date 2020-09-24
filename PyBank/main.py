39e9be74e3e412b6389d0eda41e42d3eb70107fb
Joseph Lubrano <jlubranos@optimum.net>
2020-09-24 12:13:53 -0400

    Updated Homework Sept 24

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,9 @@
 # main.py for PYBANK
+# Python Script to analyze the financial records of my ficticious company.
+#       budget_data is opened read record by record,
+#       calculations are performed during this process.
+#       After which a text file is created called PyBank_Analysis.txt,
+#       and the results are written to it. 
 import os
 import csv
 
@@ -6,6 +11,8 @@ totalmonths=0
 totalPnL=0
 firstamount=0
 change=0
+# recordholder is a list structure to be a place holder so that
+# Greatest Increase in Profits and Greatest Decrease in Profits gets calculated.
 recordholder=["",0]
 Greatest_Increase=["",0]
 Greatest_Decrease=["",0]
@@ -16,20 +23,32 @@ with open(inputpath) as inputfile:
     inputheader=next(inputrecord)
     for record in csv.reader(inputfile):
         totalmonths+=1
+# totalPnL get accumulated for as many records contained in the input file.
         totalPnL=totalPnL+float(record[1])
         if (totalmonths==1):
+# firstamount gets set to first value in dataset which is triggered when totalmonths equals 1.
             firstamount=float(record[1])
+# place holder gets set to current record
             recordholder=record
         else:
-            change=(float(recordholder[1])-float(record[1]))*-1   
+# change is calculated by subtracting previous P&L month from the current P&L month
+            change=(float(record[1])-float(recordholder[1]))
             if (change>Greatest_Increase[1]):
+# change is tested against Greatest Increase value saved from previous loop runs.
+# if true date is set and change is saved into Greastest Increase list.                
                 Greatest_Increase[0]=record[0]
                 Greatest_Increase[1]=change
             if (change<Greatest_Decrease[1]):
+# change is tested against Greatest Decrease value saved from previous loop runs.
+# if true date is set and change is saved into Greastest Decrease list.                
                 Greatest_Decrease[0]=record[0]
                 Greatest_Decrease[1]=change
             recordholder=record
-outputpath=os.path.join("Analysis","PyBank_Analysis.csv")
+
+# below a text file is opened called PyBank_Analysis.txt and the results
+# are written into it through .write.  When complete file is closed with .close()
+
+outputpath=os.path.join("Analysis","PyBank_Analysis.txt")
 outputfile=open(outputpath,"w")
 outputfile.write("Financial Analysis\n")
 outputfile.write("----------------------------\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,35 @@
-# main.py for PYPOLL
+import os
+import csv
+
+Py_Poll = {}
+Tvotes=0
+Cname=""
+inputpath=os.path.join("Resources","election_data.csv")
+with open(inputpath) as inputfile:
+    inputrecord=csv.reader(inputfile,delimiter=",")
+    inputheader=next(inputrecord)
+    for record in csv.reader(inputfile):
+        Cname=record[2]
+        if not Py_Poll.get(Cname):
+            Py_Poll[Cname]=1
+        else:
+            Py_Poll[Cname]+=1
+        Tvotes+=1
+    Sorted_Py_Poll=sorted(Py_Poll.items(), reverse=True, key=lambda x : x[1])
+outputpath=os.path.join("Analysis","PyPoll_Analysis.txt")
+outputfile=open(outputpath,"w")
+outputfile.write("Election Results\n")
+outputfile.write("-------------------------\n")
+ctvotes="{:,.0f}".format(float(Tvotes))
+outputfile.write("Total Votes : " + ctvotes + "\n")
+outputfile.write("-------------------------\n")
+for name,votes in Sorted_Py_Poll:
+    pvotes="{:.3%}".format(float(votes/Tvotes))
+    ctvotes="{:,.0f}".format(float(votes))
+    outputfile.write(name + ": " + pvotes + " (" + ctvotes + ")\n")
+outputfile.write("-------------------------\n")
+outputfile.write("Winner: " + Sorted_Py_Poll[0][0] + "\n")
+outputfile.write("-------------------------\n")
+outputfile.close()
+print("PyPoll Analysis Complete.......")
+print("Results written to PyPoll_Analysis.txt file.")
